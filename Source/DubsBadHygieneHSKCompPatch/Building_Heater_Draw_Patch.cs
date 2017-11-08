1a0cbc7647aa327580d814d3648f1ef2c7d814e4
thghca <thghca@gmail.com>
2017-11-08 22:19:35 +0500

    Initial commit

---
+++ Source/DubsBadHygieneHSKCompPatch/Building_Heater_Draw_Patch.cs
@@ -0,0 +1,49 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using Verse;
+
+namespace DubsBadHygieneHSKCompPatch
+{
+    [HarmonyPatch(typeof(DubsBadHygiene.Building_Heater))]
+    [HarmonyPatch("Draw")]
+    public static class Building_Heater_Draw_Patch
+    {
+        //if (this.fuelComp != null && this.fuelComp.HasFuel... => true && Powered(this)
+
+        //IL_0006: ldarg.0
+        //IL_0007: ldfld class ['Assembly-CSharp'] RimWorld.CompRefuelable DubsBadHygiene.Building_Heater::fuelComp => NOP
+        //IL_000C: brfalse.s IL_008D
+
+        //IL_000E: ldarg.0
+        //IL_000F: ldfld     class ['Assembly-CSharp'] RimWorld.CompRefuelable DubsBadHygiene.Building_Heater::fuelComp => NOP
+        //IL_0014: callvirt instance bool['Assembly-CSharp'] RimWorld.CompRefuelable::get_HasFuel()  call Powered
+        //IL_0019: brfalse.s IL_008D
+
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; ++i)
+            {
+                if (codes[i].opcode == OpCodes.Ldfld && (FieldInfo)codes[i].operand == typeof(DubsBadHygiene.Building_Heater).GetField(nameof(DubsBadHygiene.Building_Heater.fuelComp)))
+                {
+                    //this already on stack and it is not null (true), so just skip load field
+                    yield return new CodeInstruction(OpCodes.Nop);
+                    continue;
+                }
+                if (codes[i].opcode == OpCodes.Callvirt && (MethodInfo)codes[i].operand == typeof(CompRefuelable).GetProperty(nameof(CompRefuelable.HasFuel)).GetGetMethod())
+                {
+                    yield return new CodeInstruction(OpCodes.Call, typeof(Main).GetMethod(nameof(Main.Powered)));
+                    continue;
+                }
+                yield return codes[i];
+            }
+        }
+    }
+}
---
+++ Source/DubsBadHygieneHSKCompPatch/Main.cs
@@ -0,0 +1,34 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Verse;
+
+namespace DubsBadHygieneHSKCompPatch
+{
+    [StaticConstructorOnStartup]
+    public static class Main
+    {
+        static Main()
+        {
+            //HarmonyInstance.DEBUG = true;
+            var harmony = HarmonyInstance.Create("thghca.DubsBadHygieneHSKCompPatch");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Log.Message("DubsBadHygieneHSKCompPatch");
+        }
+
+        public static bool Powered(DubsBadHygiene.Building_Heater heater)
+        {
+            var fueledComp = heater.GetComp<SK.CompFueled>();
+            var breakdownableComp = heater.GetComp<CompBreakdownable>();
+            return (
+                fueledComp != null //&& fueledComp.fuel.count >= 0
+                && fueledComp.internalTemp > fueledComp.Props.operatingTemp
+                //&& fueledComp.compFlickable != null && fueledComp.compFlickable.SwitchIsOn
+                && breakdownableComp != null && !breakdownableComp.BrokenDown);
+        }
+    }
+}
