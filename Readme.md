456af670eac381163b4ecde293f9213e51d919f0
thghca <thghca@gmail.com>
2017-11-12 00:33:39 +0500

    PAC_AnimalCleaningPatch

---
+++ Readme.md
@@ -0,0 +1,6 @@
+#DubsBadHygieneHSKCompPatch
+Свзывает нагреватель из Dubs Hygiene с системой топлива Hardcore SK
+Уже включен в модпак. 
+#PAC_AnimalCleaningPatch
+Исправляет NRE в Pawns Are Capable у пешек-животных при уборке и копании. 
+(Housekeeper Assistant Cats и подобные моды)
---
+++ Source/PAC_AnimalCleaningPatch/PACPawn_RecordsTracker_Increment_Patch.cs
@@ -0,0 +1,41 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+
+namespace PAC_AnimalCleaningPatch
+{
+    [StaticConstructorOnStartup]
+    [HarmonyPatch(typeof(PawnsAreCapable.HarmonyPatches.Pawn_RecordsTracker_Increment))]
+    [HarmonyPatch("Prefix")]
+    public static class PACPawn_RecordsTracker_Increment_Patch
+    {
+        static PACPawn_RecordsTracker_Increment_Patch()
+        {
+            var harmony = HarmonyInstance.Create("thghca.PAC_AnimalCleaningPatch");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Log.Message("PAC_AnimalCleaningPatch");
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            //if not valid return;
+            yield return new CodeInstruction(OpCodes.Ldarg_1);
+            yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PawnsAreCapable.HarmonyPatches.Pawn_RecordsTracker_Increment), "Pawn"));
+            yield return new CodeInstruction(OpCodes.Call, typeof(PACPawn_RecordsTracker_Increment_Patch).GetMethod(nameof(ValidPawn)));
+            Label label = new Label();
+            yield return new CodeInstruction(OpCodes.Brtrue_S,label);
+            yield return new CodeInstruction(OpCodes.Ret);
+            yield return new CodeInstruction(OpCodes.Nop,label);
+            foreach (CodeInstruction instruction in instructions) yield return instruction;
+        }
+
+        public static bool ValidPawn(Pawn pawn)
+        {
+            if (pawn?.story?.traits == null || pawn?.needs?.mood?.thoughts?.memories == null) return false;
+            return true;
+        }
+    }
+}
